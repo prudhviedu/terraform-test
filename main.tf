10844c54b5662017912bd0036b8b06886df783d2
prudhviedu <38110055+prudhviedu@users.noreply.github.com>
2019-05-29 14:33:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
-  version = "~> 2.11"
+  version = "~= 2.11"
 }
 
 # Create a VPC to launch our instances into
