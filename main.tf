dd2e2c0d733c6dd918fbcd534831382eb7684a11
prudhviedu <38110055+prudhviedu@users.noreply.github.com>
2019-05-29 14:32:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
-  shared_credentials_file = "/root/.aws/credentials"
-  profile                 = "default"
+  version = "~> 2.11"
 }
 
 # Create a VPC to launch our instances into
