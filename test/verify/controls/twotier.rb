4375d602a18d42ea8d873f0abaae279ea6d8fdee
prudhviedu <38110055+prudhviedu@users.noreply.github.com>
2019-06-06 22:21:25 +0530

    Update twotier.rb

--- test/verify/controls/twotier.rb
+++ test/verify/controls/twotier.rb
@@ -1,7 +1,7 @@
 title 'two tier setups'
 
 # load data from terraform output
-content = inspec.profile.file("../files/terraform.json")
+content = inspec.profile.file("terraform.json")
 params = JSON.parse(content)
 
 INTANCE_ID = params['instance_id']['value']
