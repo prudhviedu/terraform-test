2855c381af4a78fa6290ad8d43fbd854adf452f8
prudhviedu <prudhvichanti007@gmail.com>
2019-06-10 15:50:41 +0000

    terraform test

--- build-support/run_terraform.sh
+++ build-support/run_terraform.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 echo "Applying terraform"
+cd terraform/modules
+
+export AWS_PROFILE='test-env'
+export AWS_REGION='us-east-1'
+terraform init
+echo 'yes' |terraform apply
+terraform output --json > test_cases/verify/files/terraform.json
+inspec exec test_cases/verify -t aws://eu-central-1
+echo 'yes' |terraform destroy
+#inspec exec test-cases/service_test -t ssh://ubuntu@3.88.11.51 -i /home/ubuntu/ec2-key.pem
+
