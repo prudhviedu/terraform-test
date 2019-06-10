c9cc55c96b49ecbdee26d3b9deb1999f890a9150
prudhviedu <prudhvichanti007@gmail.com>
2019-06-10 09:16:20 +0000

    Fixed the syntax error in check-effected.sh

--- build-support/check-effected.sh
+++ build-support/check-effected.sh
@@ -2,8 +2,10 @@
 commit_id=$1
 
 for var in terraform packer ansible
+do
 	git show $commit_id --name-only | grep "^$var/"
 	if [ $? -eq 0 ]; then
 		echo $var > build-support/list_effected_dir.txt
 	fi
+done
 cat build-support/list_effected_dir.txt
